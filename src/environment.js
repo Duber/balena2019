8aa01321718a12a090f78cd0854be0982dd53f50
Miguel Cruz <miguel@miguelcruz.net>
2019-10-26 18:33:34 +0200

    fixed initial envs

--- src/environment.js
+++ src/environment.js
@@ -2,9 +2,9 @@ module.exports = {
     DEVICE: process.env.DEVICE,
     SERVER_PORT: process.env.SERVER_PORT || 8080,
     SERVER_URI: process.env.SERVER_URI,
-    BALL_COLOR_R: 255,
-    BALL_COLOR_G: 0,
-    BALL_COLOR_B: 0,
-    BALL_X: 0,
-    BALL_Y: 0
+    BALL_COLOR_R: process.env.BALL_COLOR_R,
+    BALL_COLOR_G: process.env.BALL_COLOR_G,
+    BALL_COLOR_B: process.env.BALL_COLOR_B,
+    BALL_X: process.env.BALL_X,
+    BALL_Y: process.env.BALL_Y
 };
