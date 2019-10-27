b34b1f9833fe2f5ba8b5e2261f8f55ec824c3c4e
Joan Jané <jjaneballester@gmail.com>
2019-10-27 19:48:13 +0100

    Fix color env

--- src/environment.js
+++ src/environment.js
@@ -7,7 +7,7 @@ module.exports = {
     SERVER_PORT: process.env.SERVER_PORT || 8080,
     SERVER_URI: process.env.SERVER_URI,
     RATE: +(process.env.RATE || 50),
-    BALL_COLOR: (process.env.BALL_COLOR || '255,0,0').split(','),
+    BALL_COLOR: (process.env.BALL_COLOR || '255,0,0').split(',').map(i => +i),
     BALL_X: +(process.env.BALL_X || 0),
     BALL_Y: +(process.env.BALL_Y || 0)
 };
